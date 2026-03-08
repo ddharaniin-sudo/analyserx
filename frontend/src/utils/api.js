43a13d1a23a7ffdf9f80b6201d175d081d96a768
Dharani <ddharani.in@gmail.com>
2026-03-08 18:21:43 +0530

    Some changes made

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: API_URL || 'http://localhost:5000/api',
   timeout: 60000,
 });
 
